9edc3ce316e702e1bacdc7fe40054b370a6b82cf
DenisMayshev <denmay13@mail.ru>
2024-02-25 14:53:23 +0400

    Delete all coments

---
+++ iOS12-HW12/App/AppDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  AppDelegate.swift
+//  iOS12-HW12
+//
+//  Created by Ден Майшев on 23.02.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate { }
---
+++ iOS12-HW12/Sources/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  iOS12-HW12
+//
+//  Created by Ден Майшев on 23.02.2024.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+//    MARK: - UI
+
+
+//    MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupHierarchy()
+        setupLayout()
+    }
+
+//    MARK: - Setups
+
+    private func setupView() {
+
+    }
+
+    private func setupHierarchy() {
+
+    }
+
+    private func setupLayout() {
+        
+    }
+}
+
